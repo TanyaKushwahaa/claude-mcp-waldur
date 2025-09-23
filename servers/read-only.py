2e563fed8ba100dfb9796061df0ecff51ff00810
TanyaKushwahaa <KushwahaaT@cardiff.ac.uk>
2025-09-23 11:30:32 +0100

    Modify imports in read-only

--- servers/read-only.py
+++ servers/read-only.py
@@ -12,16 +12,15 @@ if error:
     print(f"Configuration error: {error}") # just to alert you when running script directly
 
 # Tools
-from ..src.waldur_get_tools import get_from_waldur, get_uuid
-from ..src.waldur_auth_tool import get_waldur_api_token
-from ..src.misc_tools import greet_user, check_query_type, infer_http_method
-from ..src.retrieve_api_endpoint_tool import retrieve_api_endpoint
+from src.waldur_get_tools import get_from_waldur, get_uuid
+from src.waldur_auth_tool import get_waldur_api_token
+from src.misc_tools import greet_user, check_query_type, infer_http_method
+from src.retrieve_api_endpoint_tool import retrieve_api_endpoint
 
 # Prompts
-from ..src.prompts import task_planner
+from src.prompts import task_planner
 
-from ..src.mcp_instance import mcp
+from src.mcp_instance import mcp
 
-import httpx
 if __name__=="__main__":
     mcp.run(transport="stdio")
